189855c2016cc95c353e1280d0c977a1b53ff165
James Amattey <me@jamattey.com>
2022-03-02 23:52:18 +0000

    Added sew view

--- recommender/movierecommender/views.py
+++ recommender/movierecommender/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 # HINT: Create a view to provide movie recommendations list for the HTML template
 
+def movie_recommendation_view(request):
+    if request.method == "GET":
+      # The context/data to be presented in the HTML template
+      context = {}
+      # Render a HTML page with specified template and context
+      return render(request, 'movierecommender/movie_list.html', context)
+
